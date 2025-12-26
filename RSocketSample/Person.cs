8d7127fdf9ab3ae8bbdb25c80b89fd966060daa1
rudty <rudtyz@gmail.com>
2025-12-26 14:34:21 +0900

    Sample net 10

--- RSocketSample/Person.cs
+++ RSocketSample/Person.cs
@@ -1,24 +1,23 @@
+namespace RSocketSample;
+
 using System;
 using System.Collections.Generic;
 using System.Text;
 using ProtoBuf;
 
-namespace RSocketSample
+[ProtoContract]
+class Person
 {
-	[ProtoContract]
-	class Person
-	{
-		[ProtoMember(1)] public int Id { get; set; }
-		[ProtoMember(2)] public string Name { get; set; }
-		[ProtoMember(3)] public Address Address { get; set; }
-		public override string ToString() => $"{Id}:{Name} ({Address})";
-	}
+	[ProtoMember(1)] public int Id { get; set; }
+	[ProtoMember(2)] public string Name { get; set; }
+	[ProtoMember(3)] public Address Address { get; set; }
+	public override string ToString() => $"{Id}:{Name} ({Address})";
+}
 
-	[ProtoContract]
-	class Address
-	{
-		[ProtoMember(1)] public string Line1 { get; set; }
-		[ProtoMember(2)] public string Line2 { get; set; }
-		public override string ToString() => $"{Line1},{Line2}";
-	}
+[ProtoContract]
+class Address
+{
+	[ProtoMember(1)] public string Line1 { get; set; }
+	[ProtoMember(2)] public string Line2 { get; set; }
+	public override string ToString() => $"{Line1},{Line2}";
 }
--- RSocketSample/Program.cs
+++ RSocketSample/Program.cs
@@ -1,3 +1,5 @@
+namespace RSocketSample;
+
 using System;
 using System.Linq;
 using System.Reactive.Linq;
@@ -5,62 +7,63 @@ using System.Threading.Tasks;
 using RSocket;
 using RSocket.Transports;
 
-namespace RSocketSample
+class Program
 {
-	class Program
+	//TODO Connection Cleanup on Unsubscribe/failure/etc
+	//TODO General Error handling -> OnError
+
+	static async Task Main(string[] args)
 	{
-		//TODO Connection Cleanup on Unsubscribe/failure/etc
-		//TODO General Error handling -> OnError
+		var loopback = new LoopbackTransport();
+		var server = new EchoServer(loopback.Beyond);
+		await server.ConnectAsync();
 
-		static async Task Main(string[] args)
+		//var client = new RSocketClient(new SocketTransport("tcp://localhost:9091/"), new RSocketOptions() { InitialRequestSize = 3 });
+		//var client = new RSocketClient(new WebSocketTransport("ws://localhost:9092/"), new RSocketOptions() { InitialRequestSize = 3 });
+		var client = new RSocketClient(loopback);
+		await client.ConnectAsync();
+
+		Console.WriteLine("Requesting Raw Protobuf Stream...");
+
+		var persondata = new Person() { Id = 1234, Name = "Someone Person", Address = new Address() { Line1 = "123 Any Street", Line2 = "Somewhere, LOC" } };
+		var personmetadata = new Person() { Id = 567, Name = "Meta Person", Address = new Address() { Line1 = "", Line2 = "" } };
+
+		//Make a Raw binary call just to show how it's done.
+		var stream = client.RequestStream(
+			resultmapper: result => (Data: ProtobufNetSerializer.Deserialize<Person>(result.data), Metadata: ProtobufNetSerializer.Deserialize<Person>(result.metadata)),
+			data: ProtobufNetSerializer.Serialize(persondata), metadata: ProtobufNetSerializer.Serialize(personmetadata));
+
+		await foreach (var persons in stream)
 		{
-			var loopback = new LoopbackTransport();
-			var server = new EchoServer(loopback.Beyond);
-			await server.ConnectAsync();
-
-			//var client = new RSocketClient(new SocketTransport("tcp://localhost:9091/"), new RSocketOptions() { InitialRequestSize = 3 });
-			//var client = new RSocketClient(new WebSocketTransport("ws://localhost:9092/"), new RSocketOptions() { InitialRequestSize = 3 });
-			var client = new RSocketClient(loopback);
-			await client.ConnectAsync();
-
-
-			Console.WriteLine("Requesting Raw Protobuf Stream...");
-
-			var persondata = new Person() { Id = 1234, Name = "Someone Person", Address = new Address() { Line1 = "123 Any Street", Line2 = "Somewhere, LOC" } };
-			var personmetadata = new Person() { Id = 567, Name = "Meta Person", Address = new Address() { Line1 = "", Line2 = "" } };
-
-			//Make a Raw binary call just to show how it's done.
-			var stream = client.RequestStream(
-				resultmapper: result => (Data: ProtobufNetSerializer.Deserialize<Person>(result.data), Metadata: ProtobufNetSerializer.Deserialize<Person>(result.metadata)),
-				data: ProtobufNetSerializer.Serialize(persondata), metadata: ProtobufNetSerializer.Serialize(personmetadata));
-
-			await stream.ForEachAsync(persons => Console.WriteLine($"RawDemo.OnNext===>[{persons.Metadata}]{persons.Data}"));
-
-
-			Console.WriteLine("\nRequesting String Serializer Stream...");
-
-			var stringclient = new RSocketClient.ForStrings(client);    //A simple client that uses UTF8 strings instead of bytes.
-			await stringclient.RequestStream("A Demo Payload")
-				.ForEachAsync(result => Console.WriteLine($"StringDemo.OnNext===>{result}"));
-
-			Console.ReadKey();
-
-			//var sender = from index in Observable.Interval(TimeSpan.FromSeconds(1)) select new Person() { Id = (int)index, Name = $"Person #{index:0000}" };
-			//using (personclient.RequestChannel(obj).Subscribe(
-			//	onNext: value => Console.WriteLine($"RequestChannel.OnNext ===>{value}"), onCompleted: () => Console.WriteLine($"RequestChannel.OnComplete!")))
-			//{
-			//	Console.ReadKey();
-			//}
+			Console.WriteLine($"RawDemo.OnNext===>[{persons.Metadata}]{persons.Data}");
 		}
+
+		Console.WriteLine("\nRequesting String Serializer Stream...");
+
+		var stringclient = new RSocketClient.ForStrings(client);    //A simple client that uses UTF8 strings instead of bytes.
+		var demoResponse = stringclient.RequestStream("A Demo Payload");
+		await foreach (var result in demoResponse)
+		{
+			Console.WriteLine($"StringDemo.OnNext===>{result}");
+		}
+
+		Console.ReadKey();
+
+		//var sender = from index in Observable.Interval(TimeSpan.FromSeconds(1)) select new Person() { Id = (int)index, Name = $"Person #{index:0000}" };
+		//using (personclient.RequestChannel(obj).Subscribe(
+		//	onNext: value => Console.WriteLine($"RequestChannel.OnNext ===>{value}"), onCompleted: () => Console.WriteLine($"RequestChannel.OnComplete!")))
+		//{
+		//	Console.ReadKey();
+		//}
 	}
+}
 
-	class EchoServer : RSocketServer
+class EchoServer : RSocketServer
+{
+	public EchoServer(IRSocketTransport transport, RSocketOptions options = default, int echoes = 2) : base(transport, options)
 	{
-		public EchoServer(IRSocketTransport transport, RSocketOptions options = default, int echoes = 2) : base(transport, options)
-		{
-			Stream(request => request,
-				request => AsyncEnumerable.Repeat(request, echoes),
-				result => result);
-		}
+		Stream(request => request,
+			request => AsyncEnumerable.Repeat(request, echoes),
+			result => result);
 	}
 }
