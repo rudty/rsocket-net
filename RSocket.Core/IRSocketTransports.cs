71e335dfd18ae48a55209a2880b036360555e936
rudty <rudtyz@gmail.com>
2026-01-08 21:09:41 +0900

    코드 정리

--- RSocket.Core/IRSocketTransports.cs
+++ RSocket.Core/IRSocketTransports.cs
@@ -1,31 +1,27 @@
-﻿using System;
+namespace RSocket;
+
 using System.IO.Pipelines;
-using System.Threading;
-using System.Threading.Tasks;
 
-namespace RSocket
+/// <summary>
+/// A Pipeline Transport for a connectable RSocket. Once connected, the Input and Output Pipelines can be used to communicate abstractly with the RSocket bytestream.
+/// </summary>
+public interface IRSocketTransport
 {
-	/// <summary>
-	/// A Pipeline Transport for a connectable RSocket. Once connected, the Input and Output Pipelines can be used to communicate abstractly with the RSocket bytestream.
-	/// </summary>
-	public interface IRSocketTransport
-	{
-		PipeReader Input { get; }
-		PipeWriter Output { get; }
+	PipeReader Input { get; }
+	PipeWriter Output { get; }
 
-		Task StartAsync(CancellationToken cancel = default);
-		Task StopAsync();
-	}
+	Task StartAsync(CancellationToken cancel = default);
+	Task StopAsync();
+}
 
-	/// <summary>
-	/// A Pipeline Transport for a serving RSocket.
-	/// </summary>
-	public interface IRSocketServerTransport
-	{
-		PipeReader Input { get; }
-		PipeWriter Output { get; }
+/// <summary>
+/// A Pipeline Transport for a serving RSocket.
+/// </summary>
+public interface IRSocketServerTransport
+{
+	PipeReader Input { get; }
+	PipeWriter Output { get; }
 
-		Task StartAsync(CancellationToken cancel = default);
-		Task StopAsync();
-	}
+	Task StartAsync(CancellationToken cancel = default);
+	Task StopAsync();
 }
--- RSocket.Core/RSocketClient.cs
+++ RSocket.Core/RSocketClient.cs
@@ -1,48 +1,40 @@
-using System;
+namespace RSocket;
+
 using System.Buffers;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 
-using IRSocketStream = System.IObserver<(System.Buffers.ReadOnlySequence<byte> metadata, System.Buffers.ReadOnlySequence<byte> data)>;
-
-namespace RSocket
+public class RSocketClient : RSocket
 {
-	public class RSocketClient : RSocket
+	Task Handler;
+	RSocketOptions Options { get; set; }
+
+	public RSocketClient(IRSocketTransport transport, RSocketOptions? options = default) : base(transport, options) { }
+
+	public Task ConnectAsync(RSocketOptions? options = default, byte[]? data = null, byte[]? metadata = null) => ConnectAsync(options ?? RSocketOptions.Default, data: data == default ? default : new ReadOnlySequence<byte>(data), metadata: metadata == default ? default : new ReadOnlySequence<byte>(metadata));
+
+	public async Task ConnectAsync(RSocketOptions options, ReadOnlySequence<byte> metadata, ReadOnlySequence<byte> data)
 	{
-		Task Handler;
-		RSocketOptions Options { get; set; }
-
-		public RSocketClient(IRSocketTransport transport, RSocketOptions? options = default) : base(transport, options) { }
-
-		public Task ConnectAsync(RSocketOptions? options = default, byte[]? data = null, byte[]? metadata = null) => ConnectAsync(options ?? RSocketOptions.Default, data: data == default ? default : new ReadOnlySequence<byte>(data), metadata: metadata == default ? default : new ReadOnlySequence<byte>(metadata));
-
-		public async Task ConnectAsync(RSocketOptions options, ReadOnlySequence<byte> metadata, ReadOnlySequence<byte> data)
-		{
-			await Transport.StartAsync();
-			Handler = Connect(CancellationToken.None);
-			await Setup(options.KeepAlive, options.Lifetime, options.MetadataMimeType, options.DataMimeType, data: data, metadata: metadata);
-		}
-
-		/// <summary>A simplfied RSocket Client that operates only on UTF-8 strings.</summary>
-		//public class ForStrings
-		//{
-		//	private readonly RSocketClient Client;
-		//	public ForStrings(RSocketClient client) { Client = client; }
-		//	public Task<string> RequestResponse(string data, string metadata = default) => Client.RequestResponse(value => Encoding.UTF8.GetString(value.data.ToArray()), new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)), metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
-		//	public IAsyncEnumerable<string> RequestStream(string data, string metadata = default)
-		//	{
-		//		return Client.RequestStream(value =>
-		//		{
-		//			return Encoding.UTF8.GetString(value.data.ToArray());
-		//		}, new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)), metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
-		//	}
-
-		//	public IAsyncEnumerable<string> RequestChannel(IAsyncEnumerable<string> inputs, string data = default, string metadata = default) =>
-		//		Client.RequestChannel(inputs, input => new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(input)), result => Encoding.UTF8.GetString(result.data.ToArray()),
-		//			data == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)),
-		//			metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
-		//}
+		await Transport.StartAsync();
+		Handler = Connect(CancellationToken.None);
+		await Setup(options.KeepAlive, options.Lifetime, options.MetadataMimeType, options.DataMimeType, data: data, metadata: metadata);
 	}
+
+	/// <summary>A simplfied RSocket Client that operates only on UTF-8 strings.</summary>
+	//public class ForStrings
+	//{
+	//	private readonly RSocketClient Client;
+	//	public ForStrings(RSocketClient client) { Client = client; }
+	//	public Task<string> RequestResponse(string data, string metadata = default) => Client.RequestResponse(value => Encoding.UTF8.GetString(value.data.ToArray()), new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)), metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
+	//	public IAsyncEnumerable<string> RequestStream(string data, string metadata = default)
+	//	{
+	//		return Client.RequestStream(value =>
+	//		{
+	//			return Encoding.UTF8.GetString(value.data.ToArray());
+	//		}, new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)), metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
+	//	}
+
+	//	public IAsyncEnumerable<string> RequestChannel(IAsyncEnumerable<string> inputs, string data = default, string metadata = default) =>
+	//		Client.RequestChannel(inputs, input => new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(input)), result => Encoding.UTF8.GetString(result.data.ToArray()),
+	//			data == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(data)),
+	//			metadata == default ? default : new ReadOnlySequence<byte>(Encoding.UTF8.GetBytes(metadata)));
+	//}
 }
